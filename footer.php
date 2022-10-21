94ae9e355f26e5f694e307bc821a0621d716b613
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-10-21 12:48:54 -0400

    add inital files

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+			<!-- STUFF -->
+		</main>
+
+
+		<footer class='site-footer'>
+		<inner-column>
+
+		<script src="scripts.js"></script>
+
+		</inner-column>
+		</footer>
+
+	</body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,6 @@
+var bars = document.querySelectorAll('p');
+
+for (bar of bars){
+	bar.classList.add('MyClass');
+}
+
